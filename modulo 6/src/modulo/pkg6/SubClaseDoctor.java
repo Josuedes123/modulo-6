0ccb96fcae0d53af8d4f9d1e3c4859a936b41386
PC2 <PC2@DESKTOP-K6GNQ7B>
2024-03-05 22:53:21 -0600

    creacon de sub clase doctor

---
+++ modulo 6/src/modulo/pkg6/SubClaseDoctor.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package modulo.pkg6;
+
+/**
+ *
+ * @author franc
+ */
+public class SubClaseDoctor extends Persona {
+    private String especialidad;
+
+    
+    public SubClaseDoctor(String nombre, String apellido, int edad, double salario, String numeroDeIdentificacion, String genero, String especialidad) {
+        super(nombre, apellido, edad, salario, numeroDeIdentificacion, genero);
+        this.especialidad = especialidad;
+    }
+
+    
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+}
